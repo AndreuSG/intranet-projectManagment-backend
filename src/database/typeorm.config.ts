0e23f66a063c076ace4dd26b1cfd4e67b6b0a908
Andreu <andreu.s.g30@gmail.com>
2025-02-11 17:58:47 +0100

    database connect

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,16 @@
+// src/database/typeorm.config.ts
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const getTypeOrmConfig = (
+  configService: ConfigService
+): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  autoLoadEntities: true,
+  synchronize: false,
+});
