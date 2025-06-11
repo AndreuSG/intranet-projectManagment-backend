e9cae3af321e1899e3a1ed87265500cc82dcfcf4
Santi Onieva <sonieva@codiara.com>
2025-06-11 17:13:13 +0200

    feat(curs_grup, schedule): Enhance query logic for fetching courses and schedules with improved joins and mapping

--- src/domain/schedule/schedule.service.ts
+++ src/domain/schedule/schedule.service.ts
@@ -3,22 +3,67 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, Not, IsNull, Like } from 'typeorm';
 import { Schedule } from './schedule.entity';
 import { CursGrup } from '../curs_grup/curs_grup.entity';
+import { CurrModul } from '../curr_modul/curr_modul.entity';
 
 @Injectable()
 export class ScheduleService {
+   private mapScheduleModul: Record<string, number> = {
+      "SEGX": 30,
+      "WEBX": 43,
+      "SERV:ASIX": 29,
+      "PRJFOL:ASIX": 52,
+      "ASOX": 28,
+      "ISOX": 51,
+      "HWX": 18,
+      "SERW": 44,
+      "INTERFW": 46,
+      "CLIW": 43,
+      "PRJFOL": 48,
+      "DESPW": 45,
+      "IPRO": 15,
+      "SEG": 10,
+      "SERV:SMX": 11,
+      "SOX": 9,
+      "CSFOL": 14,
+   };
+
    constructor(
       @InjectRepository(Schedule)
       private scheduleRepository: Repository<Schedule>,
    ) { }
 
    async findAll(): Promise<Schedule[]> {
+
+      const caseWhenClauses = Object.entries(this.mapScheduleModul).map(
+         ([sigla, modId]) => {
+            if (sigla.includes(':')) {
+               const [asig, estudio] = sigla.split(':');
+               return `WHEN schedule.assignatura = '${asig}' AND schedule.grup LIKE '${estudio}%' THEN ${modId}`;
+            } else {
+               return `WHEN schedule.assignatura = '${sigla}' THEN ${modId}`;
+            }
+         }
+      );
+      const caseSql = `
+         CASE
+            ${caseWhenClauses.join('\n      ')}
+            ELSE NULL
+         END
+      `;
+
       return this.scheduleRepository
          .createQueryBuilder('schedule')
          .where('schedule.grup != :emptyString', { emptyString: '' })
          .andWhere('schedule.aula != :emptyString', { emptyString: '' })
          .andWhere('schedule.aula NOT LIKE :aulaPattern', { aulaPattern: 'AGU%' })
          .andWhere('schedule.grup IN (:...grupNames)', { grupNames: ['ASIX2', 'SMX2B', 'SMX2C', 'SMX2A', 'DAW2'] })
-         .leftJoinAndSelect('schedule.currModul', 'currModul')
+         .andWhere('schedule.assignatura NOT LIKE :assignaturaPattern', { assignaturaPattern: 'TUT%' })
+         .leftJoinAndMapOne(
+            'schedule.currModul',    
+            CurrModul,               
+            'currModul',             
+            `currModul.id = ${caseSql}`
+         )
          .leftJoinAndSelect('schedule.user', 'user')
          .leftJoinAndMapOne(
             'schedule.cursGrup',
